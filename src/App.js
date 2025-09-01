8f33554b48e6aea3a40ff2e76a1a8247e05d4a51
MateusAkiraMuranaka <105325748+MateusAkiraMuranaka@users.noreply.github.com>
2025-09-01 20:28:59 -0300

    Create App.js
    Criando arquivo App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Benchiban Frontend</h1>
+      <p>Bem-vindo ao sistema de monitoramento de preços de placas de video</p>
+    </div>
+  );
+}
+
+export default App;
