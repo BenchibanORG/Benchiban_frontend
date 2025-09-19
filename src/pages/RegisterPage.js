f4ee6b5384a76017c0af4f7d973f2201f5f680a3
MateusAkiraMuranaka <akira.kazoku@gmail.com>
2025-09-18 23:09:43 -0300

    Adicionei testes na tela de login e melhorei o regex

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -42,7 +42,7 @@ function RegisterPage() {
     setSuccess('');
 
     // Validação de formato de e-mail adicionada
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const emailRegex = /^(?=.{1,254}$)(?=.{1,64}@)[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
     if (!emailRegex.test(email)) {
       setError('Por favor, digite um email válido.');
       return;
